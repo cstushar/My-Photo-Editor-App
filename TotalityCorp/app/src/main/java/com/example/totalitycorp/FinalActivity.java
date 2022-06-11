8858f7bdaf79c9face8a69bc404f456fc92d091c
Tushar Gupta <cstushar18@gmail.com>
2022-06-12 00:37:11 +0530

    new changes

---
+++ TotalityCorp/app/src/main/java/com/example/totalitycorp/FinalActivity.java
@@ -0,0 +1,51 @@
+package com.example.totalitycorp;
+
+import static com.example.totalitycorp.MainActivity.imgUri;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.dsphotoeditor.sdk.activity.DsPhotoEditorActivity;
+import com.dsphotoeditor.sdk.utils.DsPhotoEditorConstants;
+import com.example.totalitycorp.databinding.ActivityFinalBinding;
+
+public class FinalActivity extends AppCompatActivity {
+    ActivityFinalBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityFinalBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    // If the input image uri for DS Photo Editor is "inputImageUri", launch the editor UI
+    // using the following code
+    Intent dsPhotoEditorIntent = new Intent(this, DsPhotoEditorActivity.class);
+
+    dsPhotoEditorIntent.setData(imgUri);
+
+        // This is optional. By providing an output directory, the edited photo
+        // will be saved in the specified folder on your device's external storage;
+        // If this is omitted, the edited photo will be saved to a folder
+    dsPhotoEditorIntent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_OUTPUT_DIRECTORY, "Photo Editor");
+
+    // Optional customization: hide some tools you don't need as below
+    // int[] toolsToHide = {DsPhotoEditorActivity.TOOL_ORIENTATION, DsPhotoEditorActivity.TOOL_CROP};
+    //dsPhotoEditorIntent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_TOOLS_TO_HIDE, toolsToHide);
+}
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case 200:
+                    Uri outputUri = data.getData();
+                    // handle the result uri as you want, such as display it in an imageView;
+                    binding.imgView.setImageURI(outputUri);
+                    break;
+            }
+        }
+    }
+}
--- TotalityCorp/app/src/main/java/com/example/totalitycorp/MainActivity.java
+++ TotalityCorp/app/src/main/java/com/example/totalitycorp/MainActivity.java
@@ -1,101 +1,47 @@
 package com.example.totalitycorp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.graphics.Bitmap;
-import android.provider.MediaStore;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+import com.example.totalitycorp.databinding.ActivityMainBinding;
+import com.github.dhaval2404.imagepicker.ImagePicker;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    public static Uri imgUri;
 
-    // One Button
-    Button BSelectImage;
-    Button BCaptureImage;
-
-    // One Preview Image
-    ImageView IVPreviewImage;
-
-    // constant to compare
-    // the activity result code
-    int SELECT_PICTURE = 200;
-    int i = 0;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        // register the UI widgets with their appropriate IDs
-        BSelectImage = findViewById(R.id.BSelectImage);
-        BCaptureImage = findViewById(R.id.BCaptureImage);
-        IVPreviewImage = findViewById(R.id.IVPreviewImage);
-
-        // handle the Choose Image button to trigger
-        // the image chooser function
-        BSelectImage.setOnClickListener(new View.OnClickListener() {
+        binding.selectimage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                imageChooser();
+
+                ImagePicker.Companion.with(MainActivity.this)
+                        .crop()	    			//Crop image(Optional), Check Customization for more option
+                        .start();
             }
         });
-
-        //handle the Capture Image button to trigger
-        // the image capture function
-        BCaptureImage.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v)
-                {   i = 1;
-                    Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(camera_intent, SELECT_PICTURE);
-                }
-            });
     }
 
-    // this function is triggered when
-    // the Select Image Button is clicked
-    void imageChooser() {
-
-        // create an instance of the
-        // intent of the type image
-        Intent i = new Intent();
-        i.setType("image/*");
-        i.setAction(Intent.ACTION_GET_CONTENT);
-
-        // pass the constant to compare it
-        // with the returned requestCode
-        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
-    }
-
-    // this function is triggered when user
-    // selects the image from the imageChooser
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        if (resultCode == RESULT_OK) {
-
-            // compare the resultCode with the
-            // SELECT_PICTURE constant
-            if (requestCode == SELECT_PICTURE) {
-
-                // BitMap is data structure of image file
-                // which stor the image in memory
-                if (i == 1){
-                    Bitmap photo = (Bitmap)data.getExtras().get("data");
-                    // Set the image in imageview for display
-                    IVPreviewImage.setImageBitmap(photo);
-                }
-                else{
-                    // Get the url of the image from data
-                    Uri selectedImageUri = data.getData();
-                    if (null != selectedImageUri) {
-                        // update the preview image in the layout
-                        IVPreviewImage.setImageURI(selectedImageUri);
-                    }
-                }
+        try
+        {
+            imgUri = data.getData();
+            if (!imgUri.equals("")) {
+                startActivity(new Intent(MainActivity.this, FinalActivity.class));
             }
+        }
+        catch (Exception e){
+
         }
     }
 }
